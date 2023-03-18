873e980ca4ea26931b41b5e588e61ee062cffbe3
Abdul karim mia <97598943+abdul-karim-mia@users.noreply.github.com>
2023-03-18 21:46:34 +0530

    Update psds.js

--- bmf/psds.js
+++ bmf/psds.js
@@ -29,7 +29,7 @@ gapi.load('client', function() {
         img.setAttribute('alt', name);
         img.setAttribute('title', name);
         img.setAttribute('src', thumbnailUrl);
-        img.setAttribute('data-lity','');
+        //img.setAttribute('data-lity','');
         img.setAttribute('loading','lazy')
         div.appendChild(img);
         // Create the <button> element
