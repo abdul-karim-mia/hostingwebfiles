589e21741092bee487bf40c6a97da80ab0324511
Abdul karim mia <97598943+abdul-karim-mia@users.noreply.github.com>
2023-03-18 11:50:32 +0530

    Create psdLoader-Original.js

---
+++ bmf/psdLoader-Original.js
@@ -0,0 +1,46 @@
+// Load the Google Drive API client library
+gapi.load('client', function() {
+  // Initialize the API client with your API key
+  gapi.client.init({
+    apiKey: 'AIzaSyDC0_hBFv3L8057WG-zqygdMJH4w1Hq9hs',
+    discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest']
+  }).then(function() {
+    // Retrieve the file metadata for PSD files in the public folder
+    gapi.client.drive.files.list({
+      q: "'1m-Aw3f_8ng1KoF1SzSfOWQ_BDuncK9ZJ' in parents and mimeType='image/x-photoshop'",
+      fields: 'nextPageToken, files(id, name, thumbnailLink, webContentLink)'
+    }).then(function(response) {
+      var files = response.result.files;
+      // Iterate through the files and display them on your web page
+      for (var i = 0; i < files.length; i++) {
+        var file = files[i];
+        var url = file.webContentLink;
+        var name = file.name;
+        var thumbnailUrl = file.thumbnailLink;
+        // Create the <div> element
+        var div = document.createElement('div');
+        div.classList.add('psd');
+        div.setAttribute('data-aos', 'fade-up');
+        div.setAttribute('data-aos-duration', '1000');
+        // Create the <img> element
+        var img = document.createElement('img');
+        img.classList.add('psdThumb');
+        img.setAttribute('alt', name);
+        img.setAttribute('title', name);
+        img.setAttribute('src', thumbnailUrl);
+        div.appendChild(img);
+        // Create the <button> element
+        var button = document.createElement('button');
+        button.classList.add('dBt');
+        // Create the <a> element
+        var a = document.createElement('a');
+        a.setAttribute('href', url);
+        a.innerHTML = 'Download';
+        button.appendChild(a);
+        div.appendChild(button);
+        document.querySelector('.max-width').appendChild(div);
+        // TODO: Append the <div> element to your web page
+      }
+    });
+  });
+});
