3212372b00c2af3560b30b5b4d25929ef109d719
Abdul karim mia <97598943+abdul-karim-mia@users.noreply.github.com>
2023-04-02 21:39:29 +0530

    Update psds.js

--- bmf/psds.js
+++ bmf/psds.js
@@ -1,61 +1,52 @@
+
 // Load the Google Drive API client library
 gapi.load('client', function () {
-  // Initialize the API client with your API key
-  gapi.client.init({
-    apiKey: 'AIzaSyDC0_hBFv3L8057WG-zqygdMJH4w1Hq9hs',
-    discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest']
-  }).then(function () {
-    // Retrieve the file metadata for PSD files in the public folder
-    gapi.client.drive.files.list({
-      // https://drive.google.com/drive/folders/1RtxMFw_cDrjkW1PZ3Meu8Gm4Kjkmaa5A?usp=share_link
-      // https://drive.google.com/drive/folders/1xcmITjDd3XaD4TLRKg6BYygK5PTPSFux?usp=sharing
-      // 1m-Aw3f_8ng1KoF1SzSfOWQ_BDuncK9ZJ
-      q: "'1xcmITjDd3XaD4TLRKg6BYygK5PTPSFux' in parents and mimeType='image/x-photoshop'",
-      fields: 'nextPageToken, files(id, name, thumbnailLink, webContentLink)'
-    }).then(function (response) {
-      var files = response.result.files;
-      console.log(files)
-      // Iterate through the files and display them on your web page
-      for (var i = 0; i < files.length; i++) {
-        var file = files[i];
-        var url = file.webContentLink;
-        var name = file.name;
-        var placeHolder = "https://abdul-karim-mia.github.io/hostingwebfiles/bmf/img/Bulk%20mockups%20filler%20loading%20placeholder.gif"
-        var thumbnailUrl = file.thumbnailLink;
-        if (!thumbnailUrl) thumbnailUrl = "https://abdul-karim-mia.github.io/hostingwebfiles/bmf/img/no-data-concept-illustration_114360-536.webp";
-        // Create the <div> element
-        var div = document.createElement('div');
-        div.classList.add('psd');
-        div.setAttribute('data-aos', 'fade-up');
-        div.setAttribute('data-aos-duration', '1000');
-        // Create the <img> element
-        // var aa = document.createElement('a');
-        //aa.setAttribute('href', thumbnailUrl);
-        var img = document.createElement('img');
-        img.classList.add('psdThumb');
-        img.setAttribute('alt', name);
-        img.setAttribute('title', name);
-        img.setAttribute('src', placeHolder);
-        img.setAttribute('data-src', thumbnailUrl);
-        //img.setAttribute('onload', "this.src=this.getAttribute('data-src');");
-        // aa.setAttribute('data-lightbox', name);
-        //img.setAttribute('data-lity','');
-        img.setAttribute('loading', 'lazy')
-        //aa.appendChild(img);
-        div.appendChild(img);
-        // Create the <button> element
-        var button = document.createElement('button');
-        button.classList.add('dBt');
-        // Create the <a> element
-        var a = document.createElement('a');
-        a.setAttribute('href', url);
-        a.innerHTML = 'Download';
-        button.appendChild(a);
-        div.appendChild(button);
-        document.querySelector('.max-width').appendChild(div);
-        // TODO: Append the <div> element to your web page
-      }
-      ///////////////////////////////////////////////////////
+    // Initialize the API client with your API key
+    gapi.client.init({
+      apiKey: 'AIzaSyDC0_hBFv3L8057WG-zqygdMJH4w1Hq9hs',
+      discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest']
+    }).then(function () {
+      // Retrieve the file metadata for PSD files in the public folder
+      gapi.client.drive.files.list({
+        q: "'1xcmITjDd3XaD4TLRKg6BYygK5PTPSFux' in parents and (mimeType='image/x-photoshop' or mimeType='image/webp')",
+        fields: 'nextPageToken, files(id, name, mimeType, thumbnailLink, webContentLink)'
+      }).then(function (response) {
+        var files = response.result.files;
+        console.log(files)
+        // Iterate through the PSD files and display them on your web page
+        for (var i = 0; i < files.length; i++) {
+          var file = files[i];
+          if (file.mimeType === "image/x-photoshop") {
+            var url = file.webContentLink;
+            var name = file.name.replace(/\.[^/.]+$/, ""); // remove file extension
+            var thumbnailUrl = file.thumbnailLink;
+            // check if thumbnailUrl is undefined or empty, or if there is an image with the same name
+            if (!thumbnailUrl || thumbnailUrl === "" || !files.some(f => f.mimeType !== "image/x-photoshop" && f.name.replace(/\.[^/.]+$/, "") === name && f.thumbnailLink)) {
+              thumbnailUrl = files.find(f => f.mimeType !== "image/x-photoshop" && f.name.replace(/\.[^/.]+$/, "") === name && f.thumbnailLink)?.thumbnailLink || "https://abdul-karim-mia.github.io/hostingwebfiles/bmf/img/no-data-concept-illustration_114360-536.webp";
+            }
+            var div = document.createElement('div');
+            div.classList.add('psd');
+            div.setAttribute('data-aos', 'fade-up');
+            div.setAttribute('data-aos-duration', '1000');
+            var img = document.createElement('img');
+            img.classList.add('psdThumb');
+            img.setAttribute('alt', name);
+            img.setAttribute('title', name);
+            img.setAttribute('src', "https://abdul-karim-mia.github.io/hostingwebfiles/bmf/img/Bulk%20mockups%20filler%20loading%20placeholder.gif");
+            img.setAttribute('data-src', thumbnailUrl);
+            img.setAttribute('loading', 'lazy')
+            div.appendChild(img);
+            var button = document.createElement('button');
+            button.classList.add('dBt');
+            var a = document.createElement('a');
+            a.setAttribute('href', url);
+            a.innerHTML = 'Download';
+            button.appendChild(a);
+            div.appendChild(button);
+            document.querySelector('.max-width').appendChild(div);
+          }
+        }
+        ///////////////////////////////////////////////////////
       var psdDivs = document.getElementsByClassName("psd");
 
       if (psdDivs.length > 0) {
@@ -107,6 +98,7 @@ gapi.load('client', function () {
       }
 
       ///////////////////////////////////////////////////////
+      });
     });
   });
-});
+  
